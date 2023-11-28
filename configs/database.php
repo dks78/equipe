98af0d574a3d3538e8a17195d10fdbbbcdab54dc
Aurélien <you@example.com>
2023-11-28 09:56:39 +0100

    code de l'equipe

--- configs/database.php
+++ configs/database.php
@@ -2,13 +2,13 @@
 
 
 //$databaseConnexion= new PDO('mysql:host=127.0.0.1;dbname=car_rental', 'root', '');
+$pdo = new \PDO('mysql:host=127.0.0.1;dbname=car_rental', 'root', 'Caraboumga1.');
+    // $host = "localhost=127.0.0.1";
+    // $dbname = "car_rental";
+    // $username = "root
+    // $password = "Caraboumga1.";
 
-    $host = "localhost:3308";
-    $dbname = "car_rental";
-    $username = "root";
-    $password = "password";
 
-
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-      
+        // $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+   
 ?>
--- login.php
+++ login.php
@@ -1,97 +1,144 @@
 <?php
 
-require('./configs/database.php');
-session_start();
+// require('./configs/database.php');
+// session_start();
+// // check if button is clicked
 
-//check if button is clicked
+// if (isset($_POST['submit'])) {
 
-if (isset($_POST['submit'])) {
+//   //Check if the chebox are check
 
-  //Check if the chebox are check
+//   $logAdmin= $_POST['log_admin'];
+//   $error =false;
+//   $error_email = '';
+//   $error_password= '';
 
-  $logAdmin= $_POST['log_admin'];
-  $error =false;
-  $error_email = '';
-  $error_password= '';
+//   $email= htmlspecialchars($_POST['email']);
+//   $password= htmlspecialchars($_POST['password']);
 
-  $email= htmlspecialchars($_POST['email']);
-  $password= htmlspecialchars($_POST['password']);
-
-  if(!empty($email) AND !empty($password)){
+//   if(!empty($email) AND !empty($password)){
 
     
-  if(isset($logAdmin)){
-    //Login as Admin
+//   if(isset($logAdmin)){
+//     //Login as Admin
 
-    //Check if the email is reconize in our database*
+//     //Check if the email is reconize in our database*
 
-    $checkEmailExist = $pdo->prepare('SELECT * from admins where email = ?');
+//     $checkEmailExist = $pdo->prepare('SELECT * from admins where email = ?');
 
-    $checkEmailExist->execute(array($email));
+//     $checkEmailExist->execute(array($email));
 
-    $results = $checkEmailExist->fetch(PDO::FETCH_ASSOC);
-    var_dump( $results);
+//     $results = $checkEmailExist->fetch(PDO::FETCH_ASSOC);
+//     var_dump( $results);
 
-    if ($results) {
-      // Check if password and database password match
-      if (sha1($password) == $results['password']) {
-          $_SESSION['admin_logged'] = true;
-          $_SESSION['admin_id'] = $results['id'];
-          $_SESSION['admin_email'] = $results['email'];
-          echo 'check20';
-          header('location: ./admin/index.php');
-          exit; // It's important to exit after a header redirect
-        }else{
-          echo 'error  pasw';
-          $error=true;
-          $error_password= 'Password wrong';
-        }
-    }else{
-      echo 'error email';
-      $error= true;
-      $error_email = 'Email not reconize';
-    }
+//     if ($results) {
+//       // Check if password and database password match
+//       if (sha1($password) == $results['password']) {
+//           $_SESSION['admin_logged'] = true;
+//           $_SESSION['admin_id'] = $results['id'];
+//           $_SESSION['admin_email'] = $results['email'];
+//           echo 'check20';
+//           header('location: ./admin/index.php');
+//           exit; // It's important to exit after a header redirect
+//         }else{
+//           echo 'error  pasw';
+//           $error=true;
+//           $error_password= 'Password wrong';
+//         }
+//     }else{
+//       echo 'error email';
+//       $error= true;
+//       $error_email = 'Email not reconize';
+//     }
 
-  }else{
-    //Login as customer
-    $checkEmailExist = $pdo->prepare('SELECT * FROM customers WHERE email = ?');
-    $checkEmailExist->execute(array($email));
-    $results = $checkEmailExist->fetch(PDO::FETCH_ASSOC);
+//   }else{
+//     //Login as customer
+//     $checkEmailExist = $pdo->prepare('SELECT * FROM customers WHERE email = ?');
+//     $checkEmailExist->execute(array($email));
+//     $results = $checkEmailExist->fetch(PDO::FETCH_ASSOC);
 
-    if ($results) {
-      // Check if password and database password match
-      if (sha1($password) == $results['password']) {
-        $_SESSION['customer_logged'] = true;
-        $_SESSION['customer_id'] = $results['id'];
-        $_SESSION['customer_email'] = $results['email'];
-        var_dump( $_SESSION);
+//     if ($results) {
+//       // Check if password and database password match
+//       if (sha1($password) == $results['password']) {
+//         $_SESSION['customer_logged'] = true;
+//         $_SESSION['customer_id'] = $results['id'];
+//         $_SESSION['customer_email'] = $results['email'];
+//         var_dump( $_SESSION);
       
-        header('location: index.php');
-        exit; // It's important to exit after a header redirect
-      } else {
-        $error = true;
-        $error_password = 'Password wrong';
-      }
-    } else {
-      $error = true;
-      $error_email = 'Email not recognized';
+//         header('location: index.php');
+//         exit; // It's important to exit after a header redirect
+//       } else {
+//         $error = true;
+//         $error_password = 'Password wrong';
+//       }
+//     } else {
+//       $error = true;
+//       $error_email = 'Email not recognized';
+//     }
+//   }
+// } else {
+//   $error = true;
+//   $error_email = 'Email required';
+//   $error_password = 'Password required';
+// }
+//   }
+
+
+// Assuming you have a Database class that handles database connections
+require_once('./configs/database.php');
+class AuthController {
+    private $pdo;
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+        session_start();
     }
-  }
-} else {
-  $error = true;
-  $error_email = 'Email required';
-  $error_password = 'Password required';
+    public function login() {
+        if (isset($_POST['submit'])) {
+            $logAdmin = isset($_POST['log_admin']);
+            $error = false;
+            $error_email = '';
+            $error_password = '';
+            $email = htmlspecialchars($_POST['email']);
+            $password = htmlspecialchars($_POST['password']);
+            if (!empty($email) && !empty($password)) {
+                $this->performLogin($email, $password, $logAdmin);
+            } else {
+                $error = true;
+                $error_email = 'Email required';
+                $error_password = 'Password required';
+            }
+        }
+    }
+    private function performLogin($email, $password, $logAdmin) {
+        $tableName = $logAdmin ? 'admins' : 'customers';
+        $role = $logAdmin ? 'admin' : 'customer';
+        $checkEmailExist = $this->pdo->prepare("SELECT * FROM $tableName WHERE email = ?");
+        $checkEmailExist->execute(array($email));
+        $results = $checkEmailExist->fetch(PDO::FETCH_ASSOC);
+        if ($results) {
+            // Check if password and database password match
+            if (sha1($password) == $results['password']) {
+                $_SESSION["{$role}_logged"] = true;
+                $_SESSION["{$role}_id"] = $results['id'];
+                $_SESSION["{$role}_email"] = $results['email'];
+                header("location: ./$role/index.php");
+                exit; // It's important to exit after a header redirect
+            } else {
+                $error = true;
+                $error_password = 'Password wrong';
+            }
+        } else {
+            $error = true;
+            $error_email = 'Email not recognized';
+        }
+    }
 }
-  }
-  
-
-
-
+// Usage
+$authController = new AuthController($pdo);
+$authController->login();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -99,20 +146,14 @@ if (isset($_POST['submit'])) {
   <title>PHP Car rental Project</title>
   <link rel="stylesheet" href="./assets/css/login.css">
 </head>
-
 <body>
-
   <h1>Login To Your Dashboard</h1>
   <form method="post">
-
-  <?php
-  
-  if(isset($error)){?> 
-  
-    <div class="error"><?= $error_email ?></div>
-    <div class="error"><?= $error_password ?></div>
-  <?php }
-
+    <?php
+      if(isset($error)){?> 
+      <div class="error"><?= $error_email ?></div>
+      <div class="error"><?= $error_password ?></div>
+    <?php }
   ?>
     <div class="row">
       <label for="email">Email</label>
@@ -122,13 +163,11 @@ if (isset($_POST['submit'])) {
       <label for="password">Password</label>
       <input type="password" name="password">
     </div>
-    <div style="display: flex; color:#8086a9;
-  ">
+    <div style="display: flex; color:#8086a9;">
       <input type="checkbox" name="log_admin">
       <label for="">Log as admin</label>
     </div>
-    <div style="display: flex; color:#8086a9;
-  ">
+    <div style="display: flex; color:#8086a9;">
       <div class="links">
         <a href="/views/customer/signin" class="link">Create an account</a>
         <a href="/views/admin/registration" class="link">Admin account</a>
@@ -136,11 +175,5 @@ if (isset($_POST['submit'])) {
     </div>
     <button type="submit" name="submit">Login</button>
   </form>
-
-
-
 </body>
-
-
-
 </html>
