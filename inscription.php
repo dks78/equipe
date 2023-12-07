15065988779329325cf0ba06d0e037d0f6527b63
Aurélien <you@example.com>
2023-12-07 14:00:07 +0100

    Chnge inscription style

--- inscription.php
+++ inscription.php
@@ -3,22 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/inscription.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Inscription</title>
 </head>
 <body>
-    <div class="container mt-5">
+<main class="inscrip">
+    <div class="container mt-5 mx-auto text-center">
         <form action="save.php" method="post">
             <div class="mb-3">
                 <label for="email" class="form-label">Adresse Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Entrez votre adresse email" required>
+                <input type="email" class="form-control col-6 mx-auto" id="email" name="email" placeholder="Entrez votre adresse email" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Mot de passe</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Entrez votre mot de passe" required>
+                <input type="password" class="form-control col-6 mx-auto" id="password" name="password" placeholder="Entrez votre mot de passe" required>
             </div>
             <button type="submit" class="btn btn-primary">Inscription</button>
         </form>
     </div>
+</main>
 </body>
 </html>
